d7cfed6c6a9271a3375f180ea770c65e6b994684
cyberkingz <thony.officiel@icloud.com>
2023-07-21 20:03:51 +0200

    update font size sub header

--- src/Components/SubHeader.js
+++ src/Components/SubHeader.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './SubHeader.css'
 
 function SubHeader() {
   return (
-    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', backgroundColor: '#1e293b', padding: '10px', color: 'white'}}>
+    <div className='SubHeader' style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', backgroundColor: '#1e293b', padding: '10px', color: 'white'}}>
         <h2>Nous avons passé les trois derniers mois à tester les appareils anti-cellulite les plus vendus.</h2>
     </div>
   )
