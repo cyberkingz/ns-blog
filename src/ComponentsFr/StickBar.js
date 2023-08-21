98cae23dc110736ed845a9ba90726af8bf41435b
cyberkingz <thony.officiel@icloud.com>
2023-08-21 13:19:07 +0200

    test button

--- src/ComponentsFr/StickBar.js
+++ src/ComponentsFr/StickBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Button } from '@mui/material';
+import { AppBar, Toolbar } from '@mui/material';
 
 function StickBar() {
   return (
@@ -12,13 +12,7 @@ function StickBar() {
       }}
     >
       <Toolbar>
-        <Button
-          href="https://novalight-uk.com/products/masque-facial-avec-led-novalight"
-          variant="contained"
-          sx={{ margin: 'auto auto', backgroundColor: '#4ade80', width: '220px', fontWeight: 'bold', fontSize: '16px' }}
-        >
-        👉 The best Option
-        </Button>
+    
       </Toolbar>
     </AppBar>
   );
